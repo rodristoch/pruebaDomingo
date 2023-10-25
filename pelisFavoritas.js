fa45c352b395da5dca1f4e209af8078a675a6d0e
Jscinteractivo <jscinteractivo@gmail.com>
2023-10-25 18:11:10 -0500

    Complementen esta lista con sus pelis favoritas

---
+++ pelisFavoritas.js
@@ -0,0 +1,35 @@
+// Esta es una lista con algunas de mis pelis favoritas con datos respectivos.Les invito a que cada uno añada por lo menos 3
+
+let peliculas = [
+    {
+        nombre: 'Matrix',
+        fechaEstreno: 1999,
+        reparto:['Keanu Reeves', 'Carry-Anne Moss', 'Laurence Fishburne'],
+        director:'Wachowski sisters',
+        comentario:'Una película de culto para amantes de la tecnología',
+    },
+    {
+        nombre: 'Forrest Gump',
+        fechaEstreno: 1994,
+        reparto:['Tom Hanks', 'Robin Wright', 'Gary Sinise'],
+        director:'Robert Zemeckis',
+        comentario:'Evocadora de toda una generación de libertad',
+    },
+    {
+        nombre: 'La Historia sin Fin',
+        fechaEstreno: 1984,
+        reparto:['Noah Hathaway', 'Barret Oliver', 'Tami Stronach'],
+        director:'Wolfang Petersen',
+        comentario:'La película de mi vida',
+    },
+    {
+        nombre: 'Guasón',
+        fechaEstreno: 2019,
+        reparto:['Joaquin Phoenix', 'Robert De Niro', 'Zazie Beetz'],
+        director:'Todd Phillips',
+        comentario:'Desgarradora y maravillosa actuación',
+    }
+];
+
+/* module.exports = peliculas; */
+// Una vez añadan las suyas, exportamos la lista completa
